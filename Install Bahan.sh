1b1a0b6d3bb4c7e4b591a5b197aed804b91ab64d
Muhammad Revan <56759295+revan-ar25@users.noreply.github.com>
2019-11-06 02:54:39 +0700

    Script Install Command Termux
    Tools Ini Saya Buat Supaya Mempermudah Kalian Buat Install Command Termux

---
+++ Install Bahan.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+clear
+echo "\33[36;1m-------------------------"
+echo "\33[32;1mAuthor:\33[31;1m Revan Ar"
+echo "\33[32;1mTeam: \33[31;1m D45H7"
+echo "\33[32;1mThanks:\33[31;1m  All Mem D45H7"
+echo "\33[36;1m-------------------------"
+echo "\33[36;1m Masih Menginstall Mohon Bersabar♷...♶..♵..."
+pkg install figlet -y
+figlet install ...
+figlet UPDATE
+pkg update -y
+figlet python
+pkg install python -y
+figlet python 2
+pkg install python2 -y
+figlet python 3
+pkg install python3 -y
+figlet ruby
+pkg install ruby -y
+figlet git
+pkg install git -y
+figlet php
+pkg install php -y
+figlet java
+pkg install java -y
+figlet perl
+pkg install perl -y
+figlet nmap
+pkg install nmap -y
+figlet bash
+pkg install bash -y
+figlet clang
+pkg install clang -y
+figlet macchanger
+pkg install macchanger -y
+figlet nano
+pkg install nano -y
+figlet cowsay
+pkg install cowsay -y
+figlet curl
+pkg install curl -y
+figlet zip
+pkg install zip -y
+figlet unzip
+pkg install unzip -y
+figlet tor
+pkg install tor -y
+figlet sudo
+pkg install sudo -y
+figlet wget
+pkg install wget -y
+figlet wcalc
+pkg install wcalc -y
+figlet openssh
+pkg install openssh -y
+figlet bmon
+pkg install bmon -y
+echo "\33[36;1mTerima kasih sudah menggunakan tools receh ini"
